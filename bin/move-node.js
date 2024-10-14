1766f0336f157329892b1493610871b6bec06c30
Henok Getachew Alemayehu <henygetachew@gmail.com>
2024-10-14 23:02:27 +0300

    Remove validation for move-node bin

--- bin/move-node.js
+++ bin/move-node.js
@@ -6,7 +6,9 @@ const { moveNode, syncShards } = require('../src/move-node');
 const { removeNode } = require('../src/remove-node');
 
 const parseNodeMapping = function (input) {
-  if (input.startsWith('{')) {
+  if (!input) {
+    return undefined;
+  } else if (input.startsWith('{')) {
     return JSON.parse(input);
   } else if (input.includes(':')) {
     const [oldNode, newNode] = input.split(':');
@@ -17,20 +19,6 @@ const parseNodeMapping = function (input) {
 
 (async () => {
   try {
-    if (!toNode) {
-      console.error('Please provide the target node or node mapping.');
-      console.error('Usage for single-node migration: move-node newNode');
-      console.error('Usage for multi-node migration:');
-      console.error(
-        '  move-node \'{"oldNode":"newNode"}\'' +
-        '\'{"shards":["shard1","shard2"],"nodes":{"shard1":["oldNode"],"shard2":["oldNode"]}}\'');
-      console.error('  OR');
-      console.error(
-        '  move-node oldNode:newNode \'' +
-        '{"shards":["shard1","shard2"],"nodes":{"shard1":["oldNode"],"shard2":["oldNode"]}}\'');
-      process.exit(1);
-    }
-
     const parsedToNode = parseNodeMapping(toNode);
 
     const removedNodes = await moveNode(parsedToNode, shardMapJson);
